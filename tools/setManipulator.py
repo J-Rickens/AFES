d398591a0f1482e89af64da41ff9e979ec36f275
J-Rickens <jrickens2019@gmail.com>
2023-10-21 11:18:23 -0400

    fixed import in load function and partial testing

--- tools/setManipulator.py
+++ tools/setManipulator.py
@@ -1,7 +1,9 @@
 from tools import recTool as rt
+from tools import tempGenerator as tg
 import os
 import json
 import random
+import importlib
 
 def returnInfo(choice):
 
@@ -26,8 +28,9 @@ def returnSet(setType):
 
 	settings = {}
 	try:
-		with open(fileloc, 'r') as file_object:
-			settings = json.load(file_object)
+		file_object = open(fileloc, 'r')
+		settings = json.load(file_object)
+		file_object.close()
 	except:
 		print("Error opening file: " + fileName)
 		return "error"
@@ -47,8 +50,9 @@ def edit(setType, settings):
 	fileloc = pyloc + "\\settings\\" + fileName
 
 	try:
-		with open(fileloc, 'w') as file_object:
-			json.dump(settings, file_object)
+		file_object = open(fileloc, 'w')
+		json.dump(settings, file_object)
+		file_object.close()
 	except:
 		print("Error opening file: " + fileName)
 		return "error"
@@ -72,23 +76,25 @@ def display(setType):
 	return 1
 
 def load():
-	pyloc = os.getwd()
+	pyloc = os.getcwd()
 	settings = returnSet(1)
 	if (settings == "error"):
 		return "error"
 	fileloc = settings["locCypherList"]
+	
+	fileList = []
 	try:
-		os.chdir(fileloc)
+		fileList = os.listdir(fileloc)
 	except:
-		print("Error changing directory: " + fileloc)
+		print("Error finding directory: " + fileloc)
 		return "error"
 
-	fileList = os.listdir()
 	cypherFilesG = []
 	cypherFilesI = []
 	cypherFilesE = []
+	importflag = True
 	for file in fileList:
-		if (file == "cypherTP.py"):
+		if (file == "cypherTP.py" or file == "tempCypher.py" or file == "__init__.py"):
 			continue
 		elif (file == "rsa.py"):
 			# if correct do something
@@ -96,95 +102,100 @@ def load():
 		elif (file[-3:] == ".py"):
 			working = True
 			try:
-				import file as testCypher
-
-				flag = True
-				if (type(testCypher.returnInfo(0)) != str):
-					print("Error Name not str")
-				if (type(testCypher.returnInfo(1)) != str):
-					print("Error Description not str")
-				testKSIndicator = testCypher.returnInfo(2)
-				if (type(testKSIndicator) != bool):
-					print("Error Keep Size indicator (KSI) not boolean")
+				if (tg.createTempCypher(file[:-3]) == "error"):
+					print("Error Functional Problem loading temp")
 					working = False
-					flag = False
-				testMultiplyer = testCypher.returnInfo(3)
-				if (type(testMultiplyer) != float and type(testMultiplyer) != int):
-					print("Error Multiplyer not a Number")
-					working = False
-					flag = False
-				elif (testMultiplyer < 1):
-					print("Error unrealistic Multiplyer shoud be at least 1: " + testMultiplyer)
-				elif (testKSIndicator and testMultiplyer != 1):
-					print("Error Incorect Multiplyer for True KSI: " + testMultiplyer + " (should be 1)")
-				inputRec = testCypher.returnInfo(4)
-				outputRec = testCypher.returnInfo(5)
-				i = 0
-				while (flag or i < len(inputRec)):
-					if (type(inputRec[i]) != float and type(inputRec[i]) != int):
-						print("Error inputRec not a number")
-						working = False
-						flag = False
-					elif (type(outputRec[i]) != float and type(outputRec[i]) != int):
-						print("Error outputRec not a number")
-						working = False
-						flag = False
+				else:
+					if (importflag):
+						from cyphers import tempCypher as testCypher
+						importflag = False
 					else:
+						importlib.reload(testCypher)
 
-						testText = ""
-						testInRec = returnRec(inputRec[i])
-						for i in range(9999):
-							testText += testInRec[random.randint(0,len(testInRec)-1)]
-						testKey = random.randint(999999,999999999999999)
-
-						testcText = testCypher.encrypt(testText, testKey)
-						if (testText == testcText):
-							print("Error ecrypt did not change text")
+					flag = True
+					if (type(testCypher.returnInfo(0)) != str):
+						print("Error Name not str")
+					else:
+						print(testCypher.returnInfo(0))
+					if (type(testCypher.returnInfo(1)) != str):
+						print("Error Description not str")
+					testKSIndicator = testCypher.returnInfo(2)
+					if (type(testKSIndicator) != bool):
+						print("Error Keep Size indicator (KSI) not boolean")
+						working = False
+						flag = False
+					testMultiplyer = testCypher.returnInfo(3)
+					if (type(testMultiplyer) != float and type(testMultiplyer) != int):
+						print("Error Multiplyer not a Number")
+						working = False
+						flag = False
+					elif (testMultiplyer < 1):
+						print("Error unrealistic Multiplyer shoud be at least 1: " + testMultiplyer)
+					elif (testKSIndicator and testMultiplyer != 1):
+						print("Error Incorect Multiplyer for True KSI: " + testMultiplyer + " (should be 1)")
+					inputRec = testCypher.returnInfo(4)
+					outputRec = testCypher.returnInfo(5)
+					i = 0
+					while (flag and i < len(inputRec)):
+						if (type(inputRec[i]) != float and type(inputRec[i]) != int):
+							print("Error inputRec not a number")
 							working = False
 							flag = False
-						if (not rt.checkIfRec(testcText, outputRec[i])):
-							print("Error Incorrect outputRec")
-							working = False
-							flag = False
-						if (testKSIndicator and len(testText) != len(testcText)):
-							print("Error does not keep size when indicated")
+						elif (type(outputRec[i]) != float and type(outputRec[i]) != int):
+							print("Error outputRec not a number")
 							working = False
 							flag = False
 						else:
-							multiplerDifferential = 1 - ((len(testText)*testMultiplyer)/len(testcText))
-							margin = .1
-							if ((not testKSIndicator) and abs(multiplyerDifferential) > margin):
-								print("Error Multiplyer is outside of margin of error")
+
+							testText = ""
+							testInRec = rt.returnRec(inputRec[i])
+							for _ in range(999):
+								testText += testInRec[random.randint(0,len(testInRec)-1)]
+							testKey = random.randint(999999,999999999999999)
+
+							testcText, opo = testCypher.encrypt(testText, testKey)
+							if (testText == testcText):
+								print("Error ecrypt did not change text")
 								working = False
 								flag = False
-						
-						testOutput = testCypher.decypt(testcText, testKey)
-						if (testText != testOutput):
-							print("Error decrypt return to text")
-							working = False
-							flag = False							
-					i += 1
+							if (not rt.checkIfRec(testcText, outputRec[i])):
+								print("Error Incorrect outputRec")
+								working = False
+								flag = False
+							if (testKSIndicator and len(testText) != len(testcText)):
+								print("Error does not keep size when indicated")
+								working = False
+								flag = False
+							else:
+								multiplerDifferential = 1 - ((len(testText)*testMultiplyer)/len(testcText))
+								margin = .1
+								if ((not testKSIndicator) and abs(multiplyerDifferential) > margin):
+									print("Error Multiplyer is outside of margin of error")
+									working = False
+									flag = False
+							
+							testOutput, opo = testCypher.decrypt(testcText, testKey)
+							if (testText != testOutput):
+								print("Error decrypt return to text")
+								working = False
+								flag = False							
+						i += 1
 			except:
 				print("Error Functional Problem with cypher file")
 				working = False
 			if (working):
-				if (file in settings[1]):
-					cypherFilesI.append(file)
+				if (file[:-3] in settings["inactiveList"]):
+					cypherFilesI.append(file[:-3])
 				else:
-					cypherFilesG.append(file)
+					cypherFilesG.append(file[:-3])
 			else:
-				cypherFilesE.append(file)
+				cypherFilesE.append(file[:-3])
 		else:
 			continue
-	try:
-		os.chdir(pyloc)
-	except:
-		print("Error changing directory: " + fileloc)
-		return "error"
 
-	settings[0] = cypherFilesG
-	settings[1] = cypherFilesI
-	settings[2] = cypherFilesE
+	settings["cypherList"] = cypherFilesG
+	settings["inactiveList"] = cypherFilesI
+	settings["errorList"] = cypherFilesE
 	if (edit(1, settings) == "error"):
 		return "error"
 	elif (display(1) == "error"):
@@ -222,8 +233,9 @@ def exportSet(epath, selected):
 	fileName = fileName + str(i) + ".json"
 	
 	try:
-		with open(fileName, 'w') as file_object:
-			json.dump(allSettings, file_object)
+		file_object = open(fileName, 'w')
+		json.dump(allSettings, file_object)
+		file_object.close()
 	except:
 		print("Error opening file: " + fileName)
 		return "error"
@@ -238,8 +250,9 @@ def exportSet(epath, selected):
 def importSet(ipathfile):
 	allSettings = {}
 	try:
-		with open(ipathfile, 'r') as file_object:
-			allSettings = json.load( file_object)
+		file_object = open(ipathfile, 'r')
+		allSettings = json.load( file_object)
+		file_object.close()
 	except:
 		print("Error opening file: " + ipathfile)
 		return "error"
