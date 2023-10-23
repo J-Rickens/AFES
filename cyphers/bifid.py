01be919a51a53563d6bcbab968fb64104ede9ce9
J-Rickens <jrickens2019@gmail.com>
2023-10-23 19:37:22 -0400

    Create file for new 4 cyphers and created code for checkerboard cypher (not tested)

---
+++ cyphers/bifid.py
@@ -0,0 +1,44 @@
+# This is a template for creating/adding new cyphers for AFES
+# There are 4 defined functions that must be updated and the names cannot be changed
+
+# returnInfo is used to display info about the cypher but also has functional use
+# functional info: keepSize - does the ctext length always equals the text length (default False)
+#		sizeAdj - to what digree do the lengths differ as a multiple (default 1)
+#		inputRec - does the cypher require a spicific input like only numbers and text no symbols can be list of options (default [0])
+#		outputRec - what type of outputs are posible only numbers or can be anything, list of options equal to input (default [0])
+# inputRec and outputRec both are represented by numbers as defined bellow: (more values can be added as needed)
+# 0: any and all posible caracters in char list
+# 1: any and all posible caracters in char list except \t\n
+# 2: a-z, A-Z, 0-9
+# 3: A-Z, 0-9	.1: A-Z, 0-9, -	.2: A-Z, 0-9, :	.3: A-Z, 0-9, :, x
+# 4: 0-9	.1: 0-9, -
+def returnInfo(choice):
+
+	infoLables = ["name", "description", "keepSize", "sizeAdj", "inputRec", "outputRec"]
+	
+	info = ["name",
+		'''description''',
+		False,
+		1,
+		[0],
+		[0]]
+
+	return info[choice]
+
+# Used by encrypt and decrypt to convert the mainKey (large postive int) to what is needed for cypher
+def setKey(mainkey):
+	ckey = mainkey
+	return ckey
+
+# encrypt is a public function to edit the text into cypher text
+# opo is the output orination as in what type of input/output rec is outputed
+def encrypt(text, mainkey):
+	key = setKey(mainkey)
+	ctext = text
+	return ctext, opo
+
+# decrypt is the inverse of encrypt and is a public function
+def decrypt(ctext, mainkey):
+	key = setKey(mainkey)
+	text = ctext
+	return text, opo
---
+++ cyphers/checkerboard.py
@@ -0,0 +1,114 @@
+# Checkerboard Cypher
+from tools import setManipulator as sm
+import random
+
+# returnInfo is used to display info about the cypher but also has functional use
+# functional info: keepSize - does the ctext length always equals the text length (default False)
+#		sizeAdj - to what digree do the lengths differ as a multiple (default 1)
+#		inputRec - does the cypher require a spicific input like only numbers and text no symbols can be list of options (default [0])
+#		outputRec - what type of outputs are posible only numbers or can be anything, list of options equal to input (default [0])
+def returnInfo(choice):
+
+	infoLables = ["name", "description", "keepSize", "sizeAdj", "inputRec", "outputRec"]
+	
+	info = ["Checkerboard Cypher",
+		'''description''',
+		False,
+		2,
+		[0],
+		[1]]
+
+	return info[choice]
+
+# Used by encrypt and decrypt to convert the mainKey (large postive int) to what is needed for cypher
+def setKey(mainkey, needSeed, decrypting):
+	genSet = sm.returnSet(0)
+	charList = genSet["charList"]
+	if (needSeed):
+		random.seed(mainkey)
+
+	grid = {}
+	cols = []
+	rows = []
+
+	maxlen = int(sqrt(len(charList))) + 1
+	rowlen = random.randint(4,maxlen)
+	columnlen = len(charList)//rowlen + 1
+
+	loc = [random.randint(0,rowlen), random.randint(0,columnlen)]
+	usedLoc = []
+	for c in charList:
+	while (loc in usedLoc):
+		loc = [random.randint(0,rowlen), random.randint(0,columnlen)]
+	usedLoc.append(loc)
+	if (decrypting):
+		grid[loc] = c
+	else:
+		grid[c] = loc
+
+	modCharList = charList.replace("\n","").replace("\t","")
+	totalLen = rowlen+columnlen+2
+	letPerNum = len(modCharList)//totalLen
+	for i in range(letPerNum):
+		for j in range(totalLen):
+			c = modCharList[random.randint(0,len(modCharList))]
+			modCharList.remove(c)#--------------------------------------------------------------
+			if (j <= rowlen):
+				if (I == 0):
+					rows.append([c])
+				else:
+					rows[j].append(c)
+			else:
+				if (I == 0):
+					cols.append([c])
+				else:
+					cols[j-rowlen].append(c)
+
+	return grid, rows, cols
+
+
+# encrypt is a public function to edit the text into cypher text
+# opo is the output orination as in what type of input/output rec is outputed
+def encrypt(text, mainkey):
+	grid, rows, cols = setKey(mainkey, True, False)
+	letPerNum = len(rows[0])
+
+	ctext = ""
+	i = 0
+	for t in text:
+		temp = grid[t]
+		ctext += rows[temp[0]][random.randint(0, letPerNum-1)]
+		ctext += cols[temp[1]][random.randint(0, letPerNum-1)]
+
+		i += 1
+		if (i > random.randint(8,20)):
+			grid, rows, cols = setKey(mainkey, False, False)
+			letPerNum = len(rows[0])
+			i = 0
+
+	return ctext, 1
+
+# decrypt is the inverse of encrypt and is a public function
+def decrypt(ctext, mainkey):
+	grid, rows, cols = setKey(mainkey, True, True)
+
+	text = ""
+	i = 0
+	for j in range(0,len(ctext),2):
+		temp = []
+		for r, row in enumerate(rows):
+			if (ctext[j] in row):
+				temp.append(r)
+				break
+		for c, col in enumerate(cols):
+			if (ctext[j+1] in col):
+				temp.append(c)
+				break
+		text += grid[temp]
+
+		i += 1
+		if (i > random.randint(8,20)):
+			grid, rows, cols = setKey(mainkey, False, True)
+			i = 0
+
+	return text, 0
