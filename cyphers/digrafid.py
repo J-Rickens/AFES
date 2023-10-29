c96c560d63d3ca855a898bea7af6b27bc1c41b44
J-Rickens <jrickens2019@gmail.com>
2023-10-29 13:32:35 -0400

    Complete/Tested digrafid, need to add random key changes

--- cyphers/digrafid.py
+++ cyphers/digrafid.py
@@ -163,35 +163,66 @@ def encrypt(text, mainkey):
 
 		else:
 			interval = len(tempTable[0]) - totalIndex
-			for i in range(0, (interval-1)*3, 3):
-				r1, c1 = divmod(i,interval)
-				r2, c2 = divmod(i+1,interval)
-				r3, c3 = divmod(i+2,interval)
+			if (len(textEnd) == 0):
+				for i in range(0, interval*3, 3):
+					r1, c1 = divmod(i,interval)
+					r2, c2 = divmod(i+1,interval)
+					r3, c3 = divmod(i+2,interval)
+					temp = []
+					temp.append(tempTable[r1][totalIndex+c1])
+					temp.append(tempTable[r2][totalIndex+c2])
+					temp.append(tempTable[r3][totalIndex+c3])
+
+					for t in range(len(temp)):
+						spliter = 0
+						if (temp[t] >= size**2):
+							spliter = random.randint(temp[t]-(size**2)+1,(size**2)-1)
+						else:
+							spliter = random.randint(0,temp[t])
+						temp[t] = [spliter,temp[t]-spliter]
+					temp.insert(0,[])
+					for t in range(2):
+						t1, t2 = divmod(temp[2][t], size)
+						temp[0].append(t1)
+						temp[2][t] = t2
+
+					ctext += keyTables[1][0][temp[0][0]][temp[1][0]]
+					ctext += keyTables[1][1][temp[2][0]][temp[3][0]]
+					ctext += keyTables[2][0][temp[0][1]][temp[1][1]]
+					ctext += keyTables[2][1][temp[2][1]][temp[3][1]]
+			else:
+				for i in range(0, (interval-1)*3, 3):
+					r1, c1 = divmod(i,interval)
+					r2, c2 = divmod(i+1,interval)
+					r3, c3 = divmod(i+2,interval)
+					temp = []
+					temp.append(tempTable[r1][totalIndex+c1])
+					temp.append(tempTable[r2][totalIndex+c2])
+					temp.append(tempTable[r3][totalIndex+c3])
+
+					for t in range(len(temp)):
+						spliter = 0
+						if (temp[t] >= size**2):
+							spliter = random.randint(temp[t]-(size**2)+1,(size**2)-1)
+						else:
+							spliter = random.randint(0,temp[t])
+						temp[t] = [spliter,temp[t]-spliter]
+					temp.insert(0,[])
+					for t in range(2):
+						t1, t2 = divmod(temp[2][t], size)
+						temp[0].append(t1)
+						temp[2][t] = t2
+
+					ctext += keyTables[1][0][temp[0][0]][temp[1][0]]
+					ctext += keyTables[1][1][temp[2][0]][temp[3][0]]
+					ctext += keyTables[2][0][temp[0][1]][temp[1][1]]
+					ctext += keyTables[2][1][temp[2][1]][temp[3][1]]
+
+				r1, c1 = divmod((interval-1)*3,interval)
+				r2, c2 = divmod(((interval-1)*3)+1,interval)
 				temp = []
 				temp.append(tempTable[r1][totalIndex+c1])
 				temp.append(tempTable[r2][totalIndex+c2])
-				temp.append(tempTable[r3][totalIndex+c3])
-
-				for t in range(len(temp)):
-					spliter = 0
-					if (temp[t] >= size**2):
-						spliter = random.randint(temp[t]-(size**2)+1,(size**2)-1)
-					else:
-						spliter = random.randint(0,temp[t])
-					temp[t] = [spliter,temp[t]-spliter]
-				temp.insert(0,[])
-				for t in range(2):
-					t1, t2 = divmod(temp[2][t], size)
-					temp[0].append(t1)
-					temp[2][t] = t2
-
-				ctext += keyTables[1][0][temp[0][0]][temp[1][0]]
-				ctext += keyTables[1][1][temp[2][0]][temp[3][0]]
-				ctext += keyTables[2][0][temp[0][1]][temp[1][1]]
-				ctext += keyTables[2][1][temp[2][1]][temp[3][1]]
-
-			if (len(textEnd) > 0):
-				temp = tempTable[2][-2:]
 				for t in range(len(temp)):
 					spliter = 0
 					if (temp[t] >= size**2):
@@ -208,25 +239,6 @@ def encrypt(text, mainkey):
 				ctext += keyTables[1][0][temp[0][0]][temp[1][0]]
 				ctext += keyTables[1][1][temp[0][1]][temp[1][1]]
 				ctext += keyTables[2][0][temp[2][0]][temp[2][1]]
-			else:
-				temp = tempTable[2][-3:]
-				for t in range(len(temp)):
-					spliter = 0
-					if (temp[t] >= size**2):
-						spliter = random.randint(temp[t]-(size**2)+1,(size**2)-1)
-					else:
-						spliter = random.randint(0,temp[t])
-					temp[t] = [spliter,temp[t]-spliter]
-				temp.insert(0,[])
-				for t in range(2):
-					t1, t2 = divmod(temp[2][t], size)
-					temp[0].append(t1)
-					temp[2][t] = t2
-
-				ctext += keyTables[1][0][temp[0][0]][temp[1][0]]
-				ctext += keyTables[1][1][temp[2][0]][temp[3][0]]
-				ctext += keyTables[2][0][temp[0][1]][temp[1][1]]
-				ctext += keyTables[2][1][temp[2][1]][temp[3][1]]
 
 		totalIndex += interval
 
@@ -237,26 +249,28 @@ def decrypt(ctext, mainkey):
 	keyTables, size, _ = setKey(mainkey, True)
 	
 	textLen = len(ctext)//2
+	tableLen = textLen//2
 	textEnd = ""
 	if (textLen%2 == 1):
-		textEnd = ctext[-3]
-		text = ctext[:-3]
+		textEnd = ctext[-3:]
+		tableLen += 1
 
 	size -= 1
 	totalIndex = 0
-	ctextIndext = 0
+	ctextIndex = 0
 	tempTable = [[],[],[]]
-	while (totalIndex < textLen):
+	while (totalIndex < tableLen):
 		interval = random.randint(3,8)
-		if (totalIndex+interval < textLen):
+		if (totalIndex+interval < tableLen):
 			for i in range(0, interval*3, 3):
 				r1 = i//interval
 				r2 = (i+1)//interval
 				r3 = (i+2)//interval
-				c1 = ctext[ctextIndext]
-				c2 = ctext[ctextIndext+1]
-				c3 = ctext[ctextIndext+2]
-				c4 = ctext[ctextIndext+3]
+				c1 = ctext[ctextIndex]
+				c2 = ctext[ctextIndex+1]
+				c3 = ctext[ctextIndex+2]
+				c4 = ctext[ctextIndex+3]
+				ctextIndex += 4
 				temp = []
 				for j, row in enumerate(keyTables[1][0]):
 					if (c1 in row):
@@ -270,30 +284,129 @@ def decrypt(ctext, mainkey):
 						break
 				for j, row in enumerate(keyTables[2][0]):
 					if (c3 in row):
-						temp[0] += row.index(c1)
+						temp[0] += row.index(c3)
 						temp[1] += j*size
 						break
 				for j, row in enumerate(keyTables[2][1]):
 					if (c4 in row):
 						temp[1] += j
-						temp[2] += row.index(c2)
+						temp[2] += row.index(c4)
 						break
 				
 				tempTable[r1].append(temp[0])
-				tempTable[r2].append(temp[0])
-				tempTable[r3].append(temp[0])
-						
-			
+				tempTable[r2].append(temp[1])
+				tempTable[r3].append(temp[2])
+
+				for t in range(len(temp)):
+					if (temp[t] >= size**2):
+						random.randint(temp[t]-(size**2)+1,(size**2)-1)
+					else:
+						random.randint(0,temp[t])
+		else:
+			interval = tableLen - totalIndex
+			if (len(textEnd) == 0):
+				for i in range(0, interval*3, 3):
+					r1 = i//interval
+					r2 = (i+1)//interval
+					r3 = (i+2)//interval
+					c1 = ctext[ctextIndex]
+					c2 = ctext[ctextIndex+1]
+					c3 = ctext[ctextIndex+2]
+					c4 = ctext[ctextIndex+3]
+					ctextIndex += 4
+					temp = []
+					for j, row in enumerate(keyTables[1][0]):
+						if (c1 in row):
+							temp.append(row.index(c1))
+							temp.append(j*size)
+							break
+					for j, row in enumerate(keyTables[1][1]):
+						if (c2 in row):
+							temp[1] += j
+							temp.append(row.index(c2))
+							break
+					for j, row in enumerate(keyTables[2][0]):
+						if (c3 in row):
+							temp[0] += row.index(c3)
+							temp[1] += j*size
+							break
+					for j, row in enumerate(keyTables[2][1]):
+						if (c4 in row):
+							temp[1] += j
+							temp[2] += row.index(c4)
+							break
+					
+					tempTable[r1].append(temp[0])
+					tempTable[r2].append(temp[1])
+					tempTable[r3].append(temp[2])
+			else:
+				for i in range(0, (interval-1)*3, 3):
+					r1 = i//interval
+					r2 = (i+1)//interval
+					r3 = (i+2)//interval
+					c1 = ctext[ctextIndex]
+					c2 = ctext[ctextIndex+1]
+					c3 = ctext[ctextIndex+2]
+					c4 = ctext[ctextIndex+3]
+					ctextIndex += 4
+					temp = []
+					for j, row in enumerate(keyTables[1][0]):
+						if (c1 in row):
+							temp.append(row.index(c1))
+							temp.append(j*size)
+							break
+					for j, row in enumerate(keyTables[1][1]):
+						if (c2 in row):
+							temp[1] += j
+							temp.append(row.index(c2))
+							break
+					for j, row in enumerate(keyTables[2][0]):
+						if (c3 in row):
+							temp[0] += row.index(c3)
+							temp[1] += j*size
+							break
+					for j, row in enumerate(keyTables[2][1]):
+						if (c4 in row):
+							temp[1] += j
+							temp[2] += row.index(c4)
+							break
+					
+					tempTable[r1].append(temp[0])
+					tempTable[r2].append(temp[1])
+					tempTable[r3].append(temp[2])
+				
+				temp = []
+				r1 = ((interval-1)*3)//interval
+				r2 = (((interval-1)*3)+1)//interval
+				for j, row in enumerate(keyTables[1][0]):
+					if (textEnd[0] in row):
+						temp.append(row.index(textEnd[0]))
+						temp.append(j*size)
+						break
+				for j, row in enumerate(keyTables[1][1]):
+					if (textEnd[1] in row):
+						temp[0] += row.index(textEnd[1])
+						temp[1] += j*size
+						break
+				for j, row in enumerate(keyTables[2][0]):
+					if (textEnd[2] in row):
+						temp[1] += row.index(textEnd[2])
+						temp[1] += j
+						break
+				
+				tempTable[r1].append(temp[0])
+				tempTable[r2].append(temp[1])
+
 		totalIndex += interval
 		
 	keyTables, size, _ = setKey(mainkey, True)
 	text = ""
-	tableLen = len(tempTable[0])
-	if (textEnd != ""):
+	if (len(textEnd) > 0):
 		tableLen -= 1
 
 	for i in range(tableLen):
-		tempTable[1][i] = [divmod(tempTable[1][i], size)]
+		t1, t2 = divmod(tempTable[1][i], size)
+		tempTable[1][i] = [t1, t2]
 		text += keyTables[0][0][tempTable[1][i][0]][tempTable[0][i]]
 		text += keyTables[0][1][tempTable[1][i][1]][tempTable[2][i]]
 
@@ -302,13 +415,3 @@ def decrypt(ctext, mainkey):
 		text += keyTables[0][0][tempTable[1][-1]][tempTable[0][-1]]
 
 	return text, 0
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
