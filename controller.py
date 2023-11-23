428944c9e68d6adf28a6231420ca3a78a48deb68
J-Rickens <88795835+J-Rickens@users.noreply.github.com>
2023-11-23 18:33:32 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -20,7 +20,36 @@ def decrypterMenu(choice):
     return 0
 
 def rsakeyMenu(choice):
-    return 0
+    while (True):
+        choice = ui.menu('''\nEnter a number from the Settings menu
+                \n0: Back
+                \n1: Exit
+                \n2: Display Keys
+                \n3: Make new Keys
+                \n''')
+        
+        choice = choice.split(".")
+        if (choice[0] == 0):
+            return 0
+        elif (choice[0] == 1):
+            return 1
+        elif (choice[0] == 2):
+            choice.pop(0)
+            pubkeys, prikeys = importKeys()
+            menuStr = '''\nEnter a number from the Settings menu
+                \n0: Back
+                \n1: Exit
+                \n'''
+            for key in pubkeys:
+                menuStr += "public_" + key + "\n"
+            for key in prikeys:
+                menuStr += "private_" + key + "\n"
+            if (1 == ui.menu(menuStr)):
+                return 1
+        elif (choice[0] == 3):
+            choice.pop(0)
+            name = input("Enter the key names: ")
+            rkg.genKeys(name)
 
 def selectSetting(setChoice):
     setList = sm.returnInfo(0)
